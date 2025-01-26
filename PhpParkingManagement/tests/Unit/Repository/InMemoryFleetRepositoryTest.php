660e58c8c1773b95016ecc1ba4d348b5552555a1
David-Bouv <d.bouvencourt@gmail.com>
2025-01-26 11:01:31 +0100

    fix(fleet): add FindByVehicleId repository test unit

--- PhpParkingManagement/tests/Unit/Repository/InMemoryFleetRepositoryTest.php
+++ PhpParkingManagement/tests/Unit/Repository/InMemoryFleetRepositoryTest.php
@@ -4,7 +4,9 @@ namespace Tests\Unit\Repository;
 
 use PHPUnit\Framework\TestCase;
 use Domain\Entity\Fleet;
+use Domain\Entity\Vehicle;
 use Domain\ValueObject\FleetId;
+use Domain\ValueObject\VehicleId;
 use Infra\Repository\InMemoryFleetRepository;
 
 class InMemoryFleetRepositoryTest extends TestCase
@@ -37,6 +39,43 @@ class InMemoryFleetRepositoryTest extends TestCase
         $this->expectExceptionMessage('Fleet not found.');
 
         $this->repository->findById('non-existent-fleet');
+    }
+
+    public function testFindByVehicleIdWhenFleetExistsForVehicle()
+    {
+        // Setup: Create a fleet and vehicle
+        $vehicleId = new VehicleId('vehicle-123');
+        $fleetId = new FleetId('fleet-123');
+        $vehicle = $this->createMock(Vehicle::class);
+        $vehicle->method('getId')->willReturn($vehicleId);
+        
+        $fleet = $this->createMock(Fleet::class);
+        $fleet->method('getId')->willReturn($fleetId);
+        $fleet->method('getVehicle')->willReturn($vehicle);
+        
+        $this->repository->save($fleet);
+
+        // Test: Find the fleet by vehicle ID
+        $foundFleet = $this->repository->findByVehicleId('vehicle-123');
+
+        $this->assertSame($fleet, $foundFleet);
+    }
+
+    public function testFindByVehicleIdWhenFleetDoesNotExistForVehicle()
+    {
+        // Setup: Create a fleet but no vehicle with ID "vehicle-123"
+        $fleetId = new FleetId('fleet-123');
+        $fleet = $this->createMock(Fleet::class);
+        $fleet->method('getId')->willReturn($fleetId);
+        $fleet->method('getVehicle')->willThrowException(new \Exception("Vehicle not found in the fleet."));
+
+        $this->repository->save($fleet);
+
+        // Test: Try to find a fleet by vehicle ID when it doesn't exist
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Vehicle not found in the fleet.');
+
+        $this->repository->findByVehicleId('vehicle-123');
     }
 
     public function testSaveNewFleet()
